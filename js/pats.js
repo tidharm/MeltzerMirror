e4e1689289debc2c03a3cb4caa87b9dd4b97d0f6
Tidhar Meltzer <tidhar.meltzer@whitesourcesoftware.com>
2019-12-22 15:33:39 +0200

    Added base for using Google Maps API

--- js/pats.js
+++ js/pats.js
@@ -10,7 +10,8 @@ var ipinfoAccessToken = '';
 // Open Weather Map (obtain API Key from https://home.openweathermap.org/api_keys)
 var owmAppId = '';
 
-
+// Google Maps API Key (obtain API Key from https://console.cloud.google.com/apis/dashboard)
+var googleMapsApiKey = '';
 
 /*********************************************************************
 ======================================================================
@@ -20,3 +21,4 @@ var owmAppId = '';
 
 localStorage.setItem("ipinfoAccessTokenLS", ipinfoAccessToken);
 localStorage.setItem("owmAppIdLS", owmAppId);
+localStorage.setItem("googleMapsApiKeyLS", googleMapsApiKey);
